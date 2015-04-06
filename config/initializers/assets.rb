271810bfc45a3ddb4cf3cf751b3161e9bc4f5743
Alex Noons <alexnoons@Alexs-MacBook-Pro.local>
2015-04-06 16:37:43 -0500

    assets loaded correctly

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,15 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( jquery-1.11.0.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( jquery.backstretch.min.js )
+Rails.application.config.assets.precompile += %w( owl.carousel.min.js )
+Rails.application.config.assets.precompile += %w( jquery.magnific-popup.min.js )
+Rails.application.config.assets.precompile += %w( jquery.simple-text-rotator.min.js )
+Rails.application.config.assets.precompile += %w( jquery.waypoints.js )
+Rails.application.config.assets.precompile += %w( jquery.countTo.js )
+Rails.application.config.assets.precompile += %w( wow.min.js )
+Rails.application.config.assets.precompile += %w( smoothscroll.js )
+Rails.application.config.assets.precompile += %w( jquery.fitvids.js )
+Rails.application.config.assets.precompile += %w( custom.js )
